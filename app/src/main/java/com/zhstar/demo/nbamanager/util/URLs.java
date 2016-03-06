658ff7220c45234d082af70ff6167cc7c2b5eea7
OneChance <zhouh08@hotmail.com>
2016-03-06 22:32:03 +0800

    gradle

--- app/src/main/java/com/zhstar/demo/nbamanager/util/URLs.java
+++ app/src/main/java/com/zhstar/demo/nbamanager/util/URLs.java
@@ -4,5 +4,6 @@ package com.zhstar.demo.nbamanager.util;
  * Created by ceeg on 2015/5/11.
  */
 public class URLs {
-    public static String BASE_URL = "http://10.4.18.7/";
+    //public static String BASE_URL = "http://10.4.18.7/";
+    public static String BASE_URL = "http://192.168.1.106/";
 }
